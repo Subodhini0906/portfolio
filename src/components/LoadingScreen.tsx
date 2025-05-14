0a1e4932c4e96737a06ae1090ee7050cc8dfb262
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-05-14 18:43:58 +0530

    kn

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -47,7 +47,7 @@ const LoadingScreen=()=>{
     if(!loader) return null;
     return(
         <div ref={screenRef} className={styles.LoadingScreen}>
-            <h1 ref={nameRef} className={styles.name}>Subodhini 💻  </h1>
+            <h1 ref={nameRef} className={styles.name}>Subodhini 💻</h1>
             <div className={styles.loader}>{progress}%</div>
         </div>
     );
