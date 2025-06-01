3995a423ef2cef01377d3588a2ac1fcabb32783c
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-06-01 11:24:11 +0530

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -154,7 +154,7 @@ export default function HomePage() {
         <ScrollDownArrow />
       </section>
 
-      <section id="about" className="min-h-screen bg-black text-white flex items-center justify-center px-6 py-20">
+      <section id="about" className="min-h-screen bg-black text-white flex items-center justify-center px-6 py-10">
         <div className="max-w-6xl w-full flex flex-col md:flex-row items-center md:items-start gap-10">
           <div className='flex-1 text-center'>
           <h2 className="text-4xl font-bold mb-12 border-b-4 inline-block border-purple-500 pb-2">About Me</h2>
@@ -174,9 +174,9 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-      <section id="skills" className="min-h-screen bg-black text-white flex items-center justify-center px-6 py-20">
-  <div className="max-w-6xl w-full text-center">
-    <h2 className="text-4xl font-bold mb-12 border-b-4 inline-block border-purple-500 pb-2">Skills</h2>
+      <section id="skills" className="min-h-screen bg-black text-white flex items-center justify-center px-6 py-10">
+      <div className="max-w-6xl w-full text-center">
+     <h2 className="text-4xl font-bold mb-12 border-b-4 inline-block border-purple-500 pb-2">Skills</h2>
 
     <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-8">
       <div className="bg-zinc-900 p-6 rounded-xl shadow hover:scale-105 transition-transform duration-300">
@@ -214,8 +214,7 @@ export default function HomePage() {
       </div>
     </div>
   </div>
-</section>
-
+      </section>
     </main>
   );
 }
