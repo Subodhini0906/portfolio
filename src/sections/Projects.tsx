7fd56d29f2f3b3cf2ddc2104abf2bedd868ab8e7
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-06-01 11:38:49 +0530

    add imgs

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectSection=()=>{
+    const Projects=[
+        {
+            id:1,
+            title:"IndieVibe",
+            discription:"IndieVibe is a platform designed to help busy men and women find nearby boutiques and easily book appointments without having to visit in person. This website allows users to browse boutiques, read reviews, and secure appointments, saving them time and effort. It's built with Next.js, Tailwind CSS, and TypeScript, providing a sleek, modern, and responsive web experience.",
+            image:""
+        }
+    ]
+}
